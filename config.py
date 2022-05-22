31e5f8bde695ab8ad5eaf230a63ddb16e0a26e20
rrronly90 <rrronly90@gmail.com>
2022-05-23 00:39:25 +0400

    local local local file reference

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import datetime
 
 key_path='/Users/rawat/Downloads/credentials.json'
+key_path_local='/Users/rawat/Downloads/credentials.json'
+
 project_id="linen-compiler-351008"
 datasetid="github_pr"
 tableid="pull_request"
