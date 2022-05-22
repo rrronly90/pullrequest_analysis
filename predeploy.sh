86684aaf5cc31b61bf6e0550cf10957a4ed136e9
rrronly90 <105963344+rrronly90@users.noreply.github.com>
2022-05-22 23:43:38 +0400

    Update predeploy.sh

--- predeploy.sh
+++ predeploy.sh
@@ -1,13 +1,9 @@
 rm  -rf /tmp/deploy/
 mkdir -p /tmp/deploy/
-
 cp * /tmp/deploy/
-
 rm -rf /tmp/deploy/credentials.json
-
 file_loc=`cat config.py | grep "key" | cut -d"=" -f2`
-echo $file_loc
 
-echo `cp "$file_loc" "/tmp/deploy/"`
-
-
+comm="sudo cp ${file_loc} /tmp/deploy/"
+eval "$comm"
+sudo chmod 777 /tmp/deploy/*
