55b83f85d56d16c0792e380833b5de9425456ade
rrronly90 <105963344+rrronly90@users.noreply.github.com>
2022-05-22 21:31:31 +0400

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,24 @@
+import datetime
+import json
+from google.cloud import bigquery
+import pandas
+import pytz
+from google.oauth2 import service_account
+from config import *
+
+credentials = service_account.Credentials.from_service_account_file(key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"])
+client = bigquery.Client(credentials=credentials, project=project_id,)
+
+
+def load_data(df , project_id, datasetid, tableid, ):
+
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND",
+    )
+
+    tid = project_id+'.'+datasetid+'.'+tableid
+    job = client.load_table_from_dataframe(
+        df, tid, job_config=job_config
+    )  # Make an API request.
+    job.result()
+
