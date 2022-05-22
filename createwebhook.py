aaf18957ff279c7a04ab470c4e0c42426a635c89
rrronly90 <105963344+rrronly90@users.noreply.github.com>
2022-05-23 01:41:40 +0400

    Create createwebhook.py

---
+++ createwebhook.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import sys
+from config import *
+import sys
+payload_url = "https:"+str(sys.argv[1])
+token = str(sys.argv[2])
+
+url = "https://api.github.com/repos/"+repo_owner+"/"+repo_name+"/hooks"
+
+payload = json.dumps({
+  "name": "web",
+  "active": True,
+  "events": [
+    "pull_request"
+  ],
+  "config": {
+    "url": payload_url,
+    "content_type": "json",
+    "insecure_ssl": "0"
+  }
+})
+headers = {
+  'Accept': 'application/vnd.github.v3+json',
+  'Authorization': 'Bearer '+token,
+  'Content-Type': 'application/json',
+  'Cookie': '_octo=GH1.1.1558628448.1653205961; logged_in=no'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
