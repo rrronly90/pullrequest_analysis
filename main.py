9b4c4cc0c34c50f10ae4de00a28299426ea61ea6
rrronly90 <rrronly90@gmail.com>
2022-05-23 11:47:17 +0400

    linting

--- main.py
+++ main.py
@@ -2,28 +2,26 @@ import json
 import pandas as pd
 from helper import *
 
-
 def get_data(request):
-	if request.headers.get("content-type"):  # calling json objects
-		print('getting data ....')
-		data =request.json
-		print(type(data))
-		d= data['pull_request']
-		d['action']=data['action']
-		d['number']=data['number']
-		df=pd.DataFrame([d], columns=schema_pr)
-		df['updated_at'] = pd.to_datetime(df['updated_at'])
-		load_data(df , project_id, datasetid, tableid,"WRITE_APPEND" )
-		user= data['pull_request']['user']
-		user['action']=data['action']
-		user['number']=data['number']
-		user['pull_id']=data['pull_request']['id']
-		df_user=pd.DataFrame([user], columns=schema_user) 
-		df_user['raised_at']=df['updated_at']
-		df_user['site_admin']=df_user['site_admin'].astype(str)
-		load_data(df_user , project_id, datasetid, tableid_users,"WRITE_APPEND" )
-	else:
-		print('no data found')
-		exit(0)
-    
-
+   if request.headers.get("content-type"):
+      print('getting data ....')
+      dAta =request.json
+      print(type(dAta))
+      dD= dAta['pull_request']
+      dD['action']=dAta['action']
+      dD['number']=dAta['number']
+      dF=pd.DataFrame([dD], columns=schema_pr)
+      dF['updated_at'] = pd.to_datetime(dF['updated_at'])
+      load_data(dF , project_id, datasetid, tableid,"WRITE_APPEND")
+      
+      uSer= dAta['pull_request']['user']
+      uSer['action']=dAta['action']
+      uSer['number']=dAta['number']
+      uSer['pull_id']=dAta['pull_request']['id']
+      dF_user=pd.DataFrame([uSer], columns=schema_user)
+      dF_user['raised_at']=dF['updated_at']
+      dF_user['site_admin']=dF_user['site_admin'].astype(str)
+      load_data(dF_user , project_id, datasetid, tableid_users,"WRITE_APPEND")
+   else:
+      print('no data found')
+      exit(0)
