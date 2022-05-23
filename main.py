f5478bef9ee93b01c9f95852aff8f2365854e4c8
rrronly90 <rrronly90@gmail.com>
2022-05-23 11:52:07 +0400

    hmmmm

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from helper import *
 def get_data(request):
    if request.headers.get("content-type"):
       print('getting data ....')
-      dAta =request.json
-      print(type(dAta))
-      dD= dAta['pull_request']
-      dD['action']=dAta['action']
-      dD['number']=dAta['number']
-      dF=pd.DataFrame([dD], columns=schema_pr)
-      dF['updated_at'] = pd.to_datetime(dF['updated_at'])
-      load_data(dF , project_id, datasetid, tableid,"WRITE_APPEND")
-      
-      uSer= dAta['pull_request']['user']
-      uSer['action']=dAta['action']
-      uSer['number']=dAta['number']
-      uSer['pull_id']=dAta['pull_request']['id']
-      dF_user=pd.DataFrame([uSer], columns=schema_user)
-      dF_user['raised_at']=dF['updated_at']
-      dF_user['site_admin']=dF_user['site_admin'].astype(str)
-      load_data(dF_user , project_id, datasetid, tableid_users,"WRITE_APPEND")
+      data =request.json
+      print(type(data))
+      dd= data['pull_request']
+      dd['action']=data['action']
+      dd['number']=data['number']
+      df=pd.dataFrame([dd], columns=schema_pr)
+      df['updated_at'] = pd.to_datetime(df['updated_at'])
+      load_data(df , project_id, datasetid, tableid,"WRITE_APPEND")
+
+      user= data['pull_request']['user']
+      user['action']=data['action']
+      user['number']=data['number']
+      user['pull_id']=data['pull_request']['id']
+      df_user=pd.dataFrame([user], columns=schema_user)
+      df_user['raised_at']=df['updated_at']
+      df_user['site_admin']=df_user['site_admin'].astype(str)
+      load_data(df_user , project_id, datasetid, tableid_users,"WRITE_APPEND")
    else:
       print('no data found')
       exit(0)
