a65f272059d8f6c1e9a5af37f8de05da6e518265
rrronly90 <rrronly90@gmail.com>
2022-05-23 20:40:34 +0400

    ok list and dictionary

--- batch_loads.py
+++ batch_loads.py
@@ -10,19 +10,23 @@ import json
 import pandas as pd
 
 #generate url this is imported from configs
-url = "https://api.github.com/"+repo_owner+"/"+repo_name+"/pulls?state=all"
 
 #entry point for cloud function
 def get_data(request,context):
+	url = "https://api.github.com/repos/"+repo_owner+"/"+repo_name+"/pulls?state=all"
+	print(url)
+
 	payload={}
 	headers = {
   	'Accept': 'application/vnd.github.v3+json',
 	}
 
 	response = requests.request("GET", url, headers=headers, data=payload)
-
+	
 	data = json.loads(response.text)
-	df = pd.DataFrame(data)
+	df = pd.DataFrame([data])
+	print(df.head(5))
 
-	load_data(df_user , project_id, datasetid, tableid_batch,"WRITE_TRUNCATE" )
+	load_data(df , project_id, datasetid, tableid_batch,"WRITE_TRUNCATE" )
+
 
